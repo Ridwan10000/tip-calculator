ca7e389b3bfaa1ec6d76e478b456b4d8647759ec
Ridwan Hasan <smridwanhasan@gmail.com>
2024-08-13 22:58:38 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-window.addEventListener('load',()=>{
+window.addEventListener('load', () => {
     alert('Fill in the form and press "Enter" to see the result')
 })
 let optionButton = document.querySelectorAll('fieldset > button');
@@ -6,71 +6,75 @@ let tipInput = document.querySelector('fieldset > input[type="text"]');
 let bill = document.querySelector('.bill > input');
 let numberOfPeople = document.querySelector('.number-of-people > input');
 let tip = 0;
-for(let i = 0;i < optionButton.length;i++){
-    optionButton[i].addEventListener('click',()=>{
+for (let i = 0; i < optionButton.length; i++) {
+    optionButton[i].addEventListener('click', () => {
         optionButton[i].style.backgroundColor = "hsl(172, 67%, 45%)";
         optionButton[i].style.color = "hsl(183, 100%, 15%)";
         document.querySelector('button[type="reset"]').style.backgroundColor = "hsl(172, 67%, 45%)";
         document.querySelector('button[type="reset"]').style.color = "hsl(183, 100%, 15%)";
         document.querySelector('button[type="reset"]').style.opacity = "1";
         tip = Number(optionButton[i].value);
-        for(let j = 0; j < optionButton.length;j++){
-            if(i!=j){
+        for (let j = 0; j < optionButton.length; j++) {
+            if (i != j) {
                 optionButton[j].style.backgroundColor = " hsl(183, 100%, 15%)";
                 optionButton[j].style.color = "white";
             }
         }
+
     });
 }
-bill.addEventListener('focus',()=>{
-    bill.style. color = "hsl(183, 100%, 15%)";
+bill.addEventListener('focus', () => {
+    bill.style.color = "hsl(183, 100%, 15%)";
     document.querySelector('button[type="reset"]').style.backgroundColor = "hsl(172, 67%, 45%)"
     document.querySelector('button[type="reset"]').style.color = "hsl(183, 100%, 15%)";
     document.querySelector('button[type="reset"]').style.opacity = '1';
 });
-numberOfPeople.addEventListener('focus',()=>{
+numberOfPeople.addEventListener('focus', () => {
     numberOfPeople.style.color = "hsl(183,100%,15%)";
     document.querySelector('button[type="reset"]').style.backgroundColor = "hsl(172, 67%, 45%)"
     document.querySelector('button[type="reset"]').style.color = "hsl(183, 100%, 15%)";
     document.querySelector('button[type="reset"]').style.opacity = '1';
 });
-document.querySelector('button[type="reset"]').addEventListener('click',()=>{
-    document.querySelector('button[type="reset"]').style.backgroundColor = "hsl(186, 14%, 43%)"
-    document.querySelector('button[type="reset"]').style.color = "hsl(183, 100%, 15%)";
-    document.querySelector('button[type="reset"]').style.opacity = '0.2';
-});
-document.querySelector('.container').addEventListener("keypress", function(event) {
+document.querySelector('input[placeholder="Custom"]').addEventListener('focus',()=>{
+    for(let i = 0;i< optionButton.length;i++){
+        optionButton[i].style.backgroundColor = " hsl(183, 100%, 15%)";
+        optionButton[i].style.color = "white";
+    }
+})
+
+document.querySelector('.container').addEventListener("keypress", function (event) {
     if (event.key === "Enter") {
-        if(Number(numberOfPeople.value)!= 0){
-            if(document.querySelector('input[placeholder="Custom"]').value!=""){
-                tip = Number(document.querySelector('input[placeholder="Custom"]').value)/100;
+        if (Number(numberOfPeople.value) != 0) {
+            if (document.querySelector('input[placeholder="Custom"]').value != "") {
+                tip = Number(document.querySelector('input[placeholder="Custom"]').value) / 100;
             }
-            let tipPerHead = ((Number(bill.value * tip))/(Number(numberOfPeople.value))).toFixed(2);
-            let totalPerHead = ((Number(bill.value) + Number(bill.value) * tip)/(Number(numberOfPeople.value))).toFixed(2);
+            let tipPerHead = ((Number(bill.value * tip)) / (Number(numberOfPeople.value))).toFixed(2);
+            let totalPerHead = ((Number(bill.value) + Number(bill.value) * tip) / (Number(numberOfPeople.value))).toFixed(2);
             document.querySelector('.tip-per-person .calculated-result').innerText = tipPerHead;
             document.querySelector('.total-per-person .calculated-result').innerText = totalPerHead;
         }
-        else{
+        else {
             document.querySelector('.error-message').classList.add('active');
             document.querySelector('.number-of-people').style.outline = "1.9px solid rgba(255,0,0,0.75)"
-        }     
-        
+        }
+
     }
 });
+document.querySelector('button[type="reset"]').addEventListener('click', () => {
+    document.querySelector('button[type="reset"]').style.backgroundColor = "hsl(186, 14%, 43%)"
+    document.querySelector('button[type="reset"]').style.color = "hsl(183, 100%, 15%)";
+    document.querySelector('button[type="reset"]').style.opacity = '0.2';
+    bill.value ='0';
+    bill.style.color = "hsl(184, 14%, 56%)";
+    numberOfPeople.value = '0';
+    numberOfPeople.style.color = "hsl(184, 14%, 56%)";
+    for(let i = 0; i < optionButton.length;i++){
+        optionButton[i].style.backgroundColor = "hsl(183, 100%, 15%)";
+        optionButton[i].style.color = "white";
 
-
-
-
-
-
-
-// for(let i = 0;i < optionButton.length;i++){
-//     optionButton[i].addEventListener('mouseover',()=>{
-//         optionButton[i].style.backgroundColor = 'hsl(185, 41%, 84%)';
-//         optionButton[i].style.color = 'hsl(183, 100%, 15%)';
-//     });
-//     optionButton[i].addEventListener('mouseout',()=>{
-//         optionButton[i].style.backgroundColor = "hsl(183, 100%, 15%)";
-//         optionButton[i].style.color = "white";
-//     });
-// }
+    }
+    document.querySelector('.tip-per-person .calculated-result').innerText = '0.00';
+    document.querySelector('.total-per-person .calculated-result').innerText = '0.00';
+    document.querySelector('input[placeholder="Custom"]').value="";
+    document.querySelector('input[placeholder="Custom"]').placeholder = "Custom";
+});
